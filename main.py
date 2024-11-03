ff34a44099adfc3064d2b6bcf83f734218945dcc
aliaksei <aliaksmarchuk@gmail.com>
2024-11-03 19:38:56 +0300

    added image route{

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from controller.image.ImageController import image_router
 from controller.user.UserController import user_router
 from dao.model.user import User
 from resources.db_configuration import SessionLocal, engine
@@ -7,6 +9,7 @@ User.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(image_router)
 
 if __name__ == "__main__":
     import uvicorn
