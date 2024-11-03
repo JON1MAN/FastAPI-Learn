2354d0a98334757b72a73009cb1855bc0889520c
aliaksei <aliaksmarchuk@gmail.com>
2024-11-03 19:38:29 +0300

    changed logic

--- dao/repository/user/UserRepository.py
+++ dao/repository/user/UserRepository.py
@@ -7,6 +7,9 @@ class UserRepository:
     def get_user_by_id(self, db: Session, user_id: int) :
         return db.query(User).filter(User.id == user_id).first()
 
+    def get_user_by_email(self, db: Session, user_email: str) :
+        return db.query(User).filter(User.email == user_email).first()
+
     def get_users(self, db: Session):
         return db.query(User).all()
 
